e2f0eab684b5bc38558b52491cbf8dbd03079478
chris3es <chrisdebiase2@gmail.com>
2025-12-26 18:12:55 -0500

    Create AurexConfig.java

---
+++ AurexConfig.java
@@ -0,0 +1,15 @@
+package dev.aurex.config;
+
+public class AurexConfig {
+
+    public static boolean zoomSmooth = true;
+    public static double zoomFovMultiplier = 0.25;
+
+    public static void load() {
+        // TODO: Load from config file
+    }
+
+    public static void save() {
+        // TODO: Save to config file
+    }
+}
